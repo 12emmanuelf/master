f3158f86f951d1bc5c7a6eb5c35ccc6954be8a53
Your angaman <angamane0@gmail.com>
2024-02-02 19:41:30 +0000

    mise à jour

--- app/Http/Controllers/SinistreController.php
+++ app/Http/Controllers/SinistreController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Sinistre;
 use Illuminate\Http\Request;
 
 class SinistreController extends Controller
 {
-    //
+    public function index()
+    {
+
+        $sinistres = Sinistre::all();
+        return view('sinistre.index', compact('sinistres'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -64,15 +64,15 @@ Route::middleware('auth')->group(function () {
 
 Route::middleware('auth')->group(function () {
 
-    Route::get('/user.index', [UserController::class,'index']);
-    Route::get('/user/create', [UserController::class,'create']);
-    Route::get('/user/{id}', [UserController::class,'show']);
-    Route::get('/user/{id}/edit', [UserController::class,'edit']);
+    Route::get('/user.index', [UserController::class,'index'])->name('user.index');
+    Route::get('/user/create', [UserController::class,'create'])->name('user.create');
+    Route::get('/user/{id}', [UserController::class,'show'])->name('user.show');
+    Route::get('/user/{id}/edit', [UserController::class,'edit'])->name('user.edit');
 
 
-    Route::post('/user', [UserController::class,'store']);
-    Route::patch('/user/{id}', [UserController::class,'update']);
-    Route::delete('/user/{id}', [UserController::class,'destroy']);
+    Route::post('/user', [UserController::class,'store'])->name('user.istore');
+    Route::patch('/user/{id}', [UserController::class,'update'])->name('user.update');
+    Route::delete('/user/{id}', [UserController::class,'destroy'])->name('user.destroy');
 
 });
 
