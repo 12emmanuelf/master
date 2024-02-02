fe6bc36315067aaef4888ac0a855cc368d60a5e5
Your angaman <angamane0@gmail.com>
2024-02-02 19:52:00 +0000

    mise à jour

--- app/Http/Controllers/DossierController.php
+++ app/Http/Controllers/DossierController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Dossier;
 use Illuminate\Http\Request;
 
 class DossierController extends Controller
 {
-    //
+    public function index()
+    {
+
+        $dossiers = Dossier::all();
+        return view('dossier.index', compact('dossiers'));
+    }
 }
